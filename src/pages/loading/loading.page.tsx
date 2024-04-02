e493fe113b28fa63cc5880068ba21c6084b955b7
Rinat <r.z.zaripov@gmail.com>
2024-04-02 10:11:27 +0300

    feat: added signin, signup pages

---
+++ src/pages/loading/loading.page.tsx
@@ -0,0 +1,17 @@
+import { Helmet } from "react-helmet-async";
+import './loading.styles.css'
+import { isStatusPending } from '../../helpers';
+import { RequestStatus } from '../../const';
+
+export default function LoadingPage(): JSX.Element {
+  const isDataLoading = isStatusPending(RequestStatus.Pending);
+  return (
+    <div className="spinner-container " >
+      <Helmet>
+        <title>Загрузка страницы — Guitar-shop</title>
+      </Helmet>
+      <div className="spinner"></div>
+      <div className="text">Пожалуйста подождите . {isDataLoading ? 'Идет загрузка данных...' : 'Идет авторизация...'}</div>
+    </div>
+  )
+}
