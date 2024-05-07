8939d177ed2b99ba3f629b3525c032df54e05036
Rinat <r.z.zaripov@gmail.com>
2024-05-07 11:54:40 +0300

    fix: corrected vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 /// <reference types='vitest' />
 /// <reference types='vite/client' />
 
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]as UserConfig['plugins'],
   test: {
     globals: true,
     environment: 'jsdom',
